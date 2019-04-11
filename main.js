dfb39f842efee666eb449bd7ae8cfb2f091f79dc
syn <vicdemilly@gmail.com>
2019-04-11 15:24:00 +0200

    Correction, ajout de la condition if NaN

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-function Modulo(){
+function modulo(){
   let nbr1 = document.getElementById('firstNumber').value;
   let nbr2 = document.getElementById('secondNumber').value;
-  alert(nbr1 % nbr2);
-  // "%" est la formule modulo pour indiquer le reste d'une division
+  let result = nbr1 % nbr2;
+// "%" est la formule modulo pour indiquer le reste d'une division
+  if(isNaN(nbr1) || isNaN(nbr2)){
+    alert('Mettez des nombres !');
+  }
+  else {
+    alert(result);
+  }
 }
